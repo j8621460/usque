84a441d0b2d5df277f908bc774d97578e9f73f3e
j8621460 <j8621460@gmail.com>
2026-08-05 19:27:12 -0400

    Revert "security: add speculation store bypass mitigation"

--- internal/speculation_test.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-import "testing"
-
-func TestSpeculationControlArgs(t *testing.T) {
-	if got, want := speculationControlArgs(), (speculationControlArgsValues{option: 53, suboption: 0, disable: 4}); got != want {
-		t.Fatalf("unexpected speculation control args: got %#v want %#v", got, want)
-	}
-}
